3c35a655e562885492e0803bd92f159559cdeb50
eareyes <eareyes@redborder.com>
2022-01-05 12:55:00 +0000

    First commit

---
+++ resources/metadata.rb
@@ -0,0 +1,7 @@
+name             'rbale'
+maintainer       'redborder'
+maintainer_email 'eareyes@redborder.com'
+license          'All rights reserved'
+description      'Installs/Configures cookbook-rb-ale'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
---
+++ resources/resources/config.rb
@@ -0,0 +1,12 @@
+#Cookbook Name :: rbale
+#
+# Resource:: config
+#
+
+actions :add, :remove , :register, :deregister
+default_action :add
+
+attribute :config_dir, :kind_of => String, :default => "/etc/redborder-ale"
+attribute :name, :kind_of => String, :default => "localhost"
+attribute :ip, :kind_of => String, :default => "127.0.0.1"
+attribute :ale_nodes, :kind_of => Array, :default => []
